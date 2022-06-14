13846d65f93586afe9fd769cbc5ac6262c0fa586
ゆう✨みやした <v1xel@hotmail.com>
2022-06-14 15:00:11 +0200

    Added test character, ai controller

---
+++ Source/CO/Character/AIController/COCharacterAIController.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "COCharacterAIController.h"
+
+// Sets default values
+ACOCharacterAIController::ACOCharacterAIController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACOCharacterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACOCharacterAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/CO/Character/AnimInstance/COCharacterAnimInstance.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "COCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CO_API UCOCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool IsMoving;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool IsRunning;
+};
