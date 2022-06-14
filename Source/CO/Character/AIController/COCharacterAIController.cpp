00801493c82bdc9a2775ca4687bb6458e2ee3b34
ゆう✨みやした <v1xel@hotmail.com>
2022-06-14 15:03:54 +0200

    Updated ai controller

--- Source/CO/Character/AIController/COCharacterAIController.cpp
+++ Source/CO/Character/AIController/COCharacterAIController.cpp
@@ -1,6 +1,9 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 #include "COCharacterAIController.h"
 
 // Sets default values
@@ -8,12 +11,31 @@ ACOCharacterAIController::ACOCharacterAIController()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	//static ConstructorHelpers::FObjectFinder<UBehaviorTree> BTFinder(TEXT("BehaviorTree'/Game/CityOnline/AI/CitizenBehaviour.CitizenBehaviour'"));
+	//BehaviourTree = BTFinder.Object;
+
+	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
+	SetPerceptionComponent(*AIPerception);
+
+	SightSense = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightSense"));
+	SightSense->DetectionByAffiliation.bDetectEnemies = true;
+	SightSense->DetectionByAffiliation.bDetectNeutrals = true;
+	SightSense->DetectionByAffiliation.bDetectFriendlies = true;
+	SightSense->SightRadius = 4000;
+	SightSense->LoseSightRadius = 4500;
+	SightSense->PeripheralVisionAngleDegrees = 360.0f;
+	SightSense->AutoSuccessRangeFromLastSeenLocation = -1;
+	SightSense->SetMaxAge(0);
+	AIPerception->SetSenseEnabled(UAISenseConfig_Sight::StaticClass(), true);
+	AIPerception->ConfigureSense(*SightSense);
 }
 
 // Called when the game starts or when spawned
 void ACOCharacterAIController::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	//RunBehaviorTree(BehaviourTree);
 }
 
 // Called every frame
@@ -22,3 +44,9 @@ void ACOCharacterAIController::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+TArray<AActor*> ACOCharacterAIController::GetPerceivedActors(TSubclassOf<UAISense> SenseToUse)
+{
+	TArray<AActor*> ActorsSensed;
+	AIPerception->GetPerceivedActors(SenseToUse, ActorsSensed);
+	return ActorsSensed;
+}
--- Source/CO/Character/AIController/COCharacterAIController.h
+++ Source/CO/Character/AIController/COCharacterAIController.h
@@ -6,6 +6,9 @@
 #include "AIController.h"
 #include "COCharacterAIController.generated.h"
 
+class UAISenseConfig_Sight;
+class UAISense;
+
 UCLASS()
 class CO_API ACOCharacterAIController : public AAIController
 {
@@ -22,4 +25,16 @@ protected:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+
+protected:
+	UBehaviorTree* BehaviourTree;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Test)
+	UAIPerceptionComponent* AIPerception;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Test)
+	UAISenseConfig_Sight* SightSense;
+
+	TArray<AActor*> GetPerceivedActors(TSubclassOf<UAISense> SenseToUse);
 };
