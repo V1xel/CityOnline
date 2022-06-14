b2e6b25ec0da5f93eb9a5d47d6db2b6cfdc93702
ゆう✨みやした <v1xel@hotmail.com>
2022-06-14 12:47:27 +0200

    Added base project files

---
+++ README.md
@@ -0,0 +1,3 @@
+# CO
+
+Developed with Unreal Engine 5
---
+++ Source/CO/COGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "COGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CO_API ACOGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
