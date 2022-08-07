63d4c59495cd53cd56ded71d6e9598133d5a4077
ゆう✨みやした <v1xel@hotmail.com>
2022-08-07 10:30:49 +0200

    Added game instance

---
+++ Source/CO/Game/COGameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "COGameInstance.generated.h"
+
+class UCORootAsset;
+
+/**
+ * 
+ */
+UCLASS()
+class CO_API UCOGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UCORootAsset* RootAsset;
+};
