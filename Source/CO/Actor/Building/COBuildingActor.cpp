be887652eae43cfe62048c4f2b735e14709d45d0
Vitalii Koliesnik <v1xel@outlook.de>
2023-03-02 01:53:47 +0100

    Implemented tag asset interface for building actor

--- Source/CO/Actor/Building/COBuildingActor.cpp
+++ Source/CO/Actor/Building/COBuildingActor.cpp
@@ -16,6 +16,26 @@ void ACOBuildingActor::BeginPlay()
 	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
 
+void ACOBuildingActor::GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const
+{
+	AbilitySystemComponent->GetOwnedGameplayTags(TagContainer);
+}
+
+bool ACOBuildingActor::HasMatchingGameplayTag(FGameplayTag TagToCheck) const
+{
+	return AbilitySystemComponent->HasMatchingGameplayTag(TagToCheck);
+}
+
+bool ACOBuildingActor::HasAllMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const
+{
+	return AbilitySystemComponent->HasAllMatchingGameplayTags(TagContainer);
+}
+
+bool ACOBuildingActor::HasAnyMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const
+{
+	return AbilitySystemComponent->HasAnyMatchingGameplayTags(TagContainer);
+}
+
 void ACOBuildingActor::ComposeBuilding(int32 Floors, UCOBuildingAsset* BuildingAsset, FVector Direction)
 {
 	UCOBuildingFunctionLibrary::ComposeBuilding(this, Floors, BuildingPartComponentClass, BuildingAsset, Direction, Meshes);
--- Source/CO/Actor/Building/COBuildingActor.h
+++ Source/CO/Actor/Building/COBuildingActor.h
@@ -6,14 +6,15 @@
 #include "AbilitySystemInterface.h"
 #include "Attributes/COBuildingAttributeSet.h"
 #include "GameFramework/Actor.h"
+#include "GameplayTagAssetInterface.h"
 #include "COBuildingActor.generated.h"
 
 class UCOBuildingPartComponent;
 class UCOConstructionDTO;
 class UCOBuildingAsset;
 
-UCLASS(Blueprintable)
-class CO_API ACOBuildingActor : public AActor, public IAbilitySystemInterface
+UCLASS(Abstract, Blueprintable)
+class CO_API ACOBuildingActor : public AActor, public IAbilitySystemInterface, public IGameplayTagAssetInterface
 {
 	GENERATED_BODY()
 
@@ -26,6 +27,14 @@ public:
 	{
 		return AbilitySystemComponent;
 	}
+
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+
+	virtual bool HasMatchingGameplayTag(FGameplayTag TagToCheck) const override;
+
+	virtual bool HasAllMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const override;
+
+	virtual bool HasAnyMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const override;
 
 	void ComposeBuilding(int32 Floors, UCOBuildingAsset* BuildingAsset, FVector Direction);
 	
