ba0b7383154e5a9a5db21496840121931d10d273
Vitalii Koliesnik <v1xel@outlook.de>
2023-03-07 16:58:01 +0100

    Added shopping list handling

--- Source/CO/Actor/Character/COCitizenCharacter.h
+++ Source/CO/Actor/Character/COCitizenCharacter.h
@@ -41,6 +41,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintCallable)
 	void RegenerateShoppingList();
 public:
 	UPROPERTY(BlueprintReadOnly)
